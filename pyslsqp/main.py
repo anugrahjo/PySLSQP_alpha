a89f5d5f6da180fcc3b111c2d479ba8a585715a3
Anugrah Jo Joshy <44123535+anugrahjo@users.noreply.github.com>
2024-05-22 22:00:52 -0700

    Trial changes made did not work so going back to previous version, i.e., no import of _slsqp module

--- pyslsqp/main.py
+++ pyslsqp/main.py
@@ -37,13 +37,6 @@ if os.getenv('READTHEDOCS') == 'False':
 else:
     slsqp = None
 
-# Another workaround for ReadTheDocs to avoid the ImportError
-# The following will also work since the compiled module can be imported 
-# as _slsqp from the build directory rather than ._slsqp from the package directory.
-# since the compiled module is available in the package directory.
-if slsqp is None:
-    from pyslsqp._slsqp import slsqp
-
 from .visualize import Visualizer
 # from visualize_plotly import Visualizer
 # from visualize_plotly_tabs import Visualizer
